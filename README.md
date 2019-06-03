2219ae9dbb920e43f5039ce62e88080a7111ef1a
Maxim Gusev <mgusev@rpkb.ru>
2019-06-03 17:04:29 +0300

    Initial commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .travis.yml
    	new file:   CMakeLists.txt
    	new file:   Doxyfile
    	new file:   README.md
    	new file:   src/CMakeLists.txt
    	new file:   src/main.cpp

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lab 6
+[![Build Status](https://travis-ci.org/John-Jasper-Doe/Lab6.svg?branch=master)](https://travis-ci.com/John-Jasper-Doe/Lab6)
+[ ![Download](https://api.bintray.com/packages/john-jasper-doe/otus-cpp/homeworks/images/download.svg?version=bulk) ](https://bintray.com/john-jasper-doe/otus-cpp/homeworks/bulk/link)
+
+## Otus Developer C++ Course
+Homework "BULK"
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/**
+ ******************************************************************************
+ * @file    main.cpp
+ * @author  Maxim <aveter@bk.ru>
+ * @date    03/06/2019
+ * @brief   The main project file is "Bulk".
+ ******************************************************************************
+ */
+
+
+#include <iostream>
+
+
+/**
+ * @brief Main function / entry point.
+ */
+int main()
+{
+
+  return 0;
+}
+
