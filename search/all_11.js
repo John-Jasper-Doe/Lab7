b18508dcdd2b313fbae3ab30d09bca7c2440a933
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-24 23:13:09 +0000

    Deploy John-Jasper-Doe/Lab6 to github.com/John-Jasper-Doe/Lab6.git:gh-pages

--- search/all_11.js
+++ search/all_11.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['_7econtroller',['~controller',['../classjjd_1_1controller.html#afcdf9813e89ec920b6899f923a47e8ef',1,'jjd::controller']]],
+  ['_7econtroller',['~controller',['../classjjd_1_1controller.html#a610e53e9878e9a430a9ad87ab04df556',1,'jjd::controller']]],
   ['_7elogger',['~logger',['../classjjd_1_1logger.html#ae7e366e57633cd47fb7ee272fb69386b',1,'jjd::logger']]],
   ['_7eobserver',['~observer',['../classjjd_1_1observer.html#a300a80d5a3d49c3ce2f727ea53d8209d',1,'jjd::observer']]],
   ['_7epool',['~pool',['../classjjd_1_1pool.html#a35418f130611616e285a97af69cb52e5',1,'jjd::pool']]],
--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['init_5flogger_5f',['init_logger_',['../classjjd_1_1controller.html#a8d18b89053b0250b7e286ebd869e4e1e',1,'jjd::controller']]],
   ['input_5fstream_5f',['input_stream_',['../classjjd_1_1reader.html#a4701ca1d2d3f20e2c80c7f0c0815dea5',1,'jjd::reader']]]
 ];
